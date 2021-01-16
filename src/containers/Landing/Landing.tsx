bc88f52a98a994a10bc823c89490188c60a03c5c
AJ Clemente <areljann.c@gmail.com>
2021-01-15 20:05:14 -0500

    DEVELOP: Add button that links to Twitch OAuth

--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -2,14 +2,7 @@ import React, { FC, Fragment, useState, ChangeEvent, memo } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
-import {
-  Flex,
-  Header,
-  TextField,
-  TextFieldLabel,
-  TextFieldInput,
-  Button
-} from '../../shared/components'
+import { Flex, Header, TextField, TextFieldLabel, TextFieldInput, Button } from '../../shared/components'
 import { isValidTwitchUsername, encrypt, encode } from '../../shared/utils'
 
 // Component
@@ -17,15 +10,11 @@ const Landing: FC = () => {
   const router = useRouter()
   const [twitchUsername, setTwitchUsername] = useState<string>('')
   const [twitchToken, setTwitchToken] = useState<string>('')
-  const [hasErrorTwitchUsername, setHasErrorTwitchUsername] = useState<boolean>(
-    false
-  )
+  const [hasErrorTwitchUsername, setHasErrorTwitchUsername] = useState<boolean>(false)
 
   const handleTwitchUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
     const username = event.target.value
-    isValidTwitchUsername(username)
-      ? setHasErrorTwitchUsername(false)
-      : setHasErrorTwitchUsername(true)
+    isValidTwitchUsername(username) ? setHasErrorTwitchUsername(false) : setHasErrorTwitchUsername(true)
     setTwitchUsername(username)
   }
 
@@ -47,9 +36,7 @@ const Landing: FC = () => {
         <Flex flexDirection='column'>
           <Header>Twitch FireTrash</Header>
           <TextField>
-            <TextFieldLabel hasError={hasErrorTwitchUsername}>
-              Twitch Username
-            </TextFieldLabel>
+            <TextFieldLabel hasError={hasErrorTwitchUsername}>Twitch Username</TextFieldLabel>
             <TextFieldInput
               name='twitch_username_textfield'
               value={twitchUsername}
@@ -59,18 +46,18 @@ const Landing: FC = () => {
           </TextField>
           <TextField>
             <TextFieldLabel>Twitch OAuth Token</TextFieldLabel>
-            <TextFieldInput
-              name='twitch_token_textfield'
-              value={twitchToken}
-              onChange={handleTwitchTokenChange}
-            />
+            <TextFieldInput name='twitch_token_textfield' value={twitchToken} onChange={handleTwitchTokenChange} />
           </TextField>
-          <Button
-            onClick={handleGoToWidgetUsernameTokenPage}
-            disabled={hasErrorTwitchUsername}
-          >
+          <Button onClick={handleGoToWidgetUsernameTokenPage} disabled={hasErrorTwitchUsername}>
             Go to extension
           </Button>
+          <div style={{ margin: '16px' }}>
+            <Button>
+              <a href='https://twitchapps.com/tmi/' style={{ textDecoration: 'none', color: 'white' }} target='_blank'>
+                Get your OAuth token
+              </a>
+            </Button>
+          </div>
         </Flex>
       </LandingWrapper>
     </Fragment>
